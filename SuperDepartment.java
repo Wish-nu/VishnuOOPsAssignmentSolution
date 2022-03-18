7d360284f662f2b0ba299f66e33ae3f4c414faca
Wish-nu <81408708+Wish-nu@users.noreply.github.com>
2022-03-18 18:35:21 +0530

    SuperDepartment.java

---
+++ SuperDepartment.java
@@ -0,0 +1,17 @@
+package com.support.mycompany;
+
+public class SuperDepartment {
+    String departmentName(){
+        return "Super Department";
+    }
+    String getTodaysWork(){
+        return "No work as of now";
+    }
+    String getWorkDeadline(){
+        return "Nil";
+    }
+    String isTodayAHoliday(){
+        return "Today is not a holiday";
+    }
+
+}
